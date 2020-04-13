4956524182d40b7445df6bda4b3e6ca5350ca99e
Chris Umphlett <christopher.umphlett@gmail.com>
2020-04-13 16:32:54 -0400

    First commit with working version of authentication and get_contact_events functions, as well as all supporting files.

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# ibmAcousticR 0.0.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
+* Created first group of functions that allow one to authenticate and query for email contact event data.
---
+++ R/utils.R
@@ -0,0 +1,42 @@
+#' Get Job Id of Submitted Job
+#' 
+#' This function is called by other functions that submit jobs to
+#' the Acoustic/Silverpop API. It extracts the Job Id from the XML
+#' returned by the API call.
+#'
+#' Job results are available as exports in the Silverpop portal by
+#' going to Resources -> Data Jobs.
+#' 
+#' It is not recommended that these authentication parameters be 
+#' stored directly in your code. There are various methods and 
+#' packages available that are more secure; this package does not 
+#' require you to use any one in particular.
+#' 
+#' @param request_obj Name of the object returned from API call,
+#' should always be "request".
+#' 
+#' @importFrom httr "content"
+#' @importFrom XML "xmlParse"
+#' @importFrom XML "xmlValue"
+#' @importFrom XML "xpathSApply"
+#' 
+#' @return A vector with the Job Id.
+#' 
+#' @keywords internal
+#' 
+#' @examples
+#' \dontrun{
+#' job_id <- get_job_id(request)
+#' }
+
+
+get_job_id <- function(request_obj) {
+  # Extract the XML from the request results
+  request_content <- httr::content(request_obj, "text", encoding = "ISO-8859-1")
+  request_xml <- XML::xmlParse(request_content)
+  
+  # Return the job id
+  job_id <- XML::xpathSApply(request_xml, "//Envelope/Body/RESULT/MAILING/JOB_ID", XML::xmlValue)
+  print(paste0("Submit was successful, Job Id: ", job_id))
+  return(job_id)
+}
